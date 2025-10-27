d2cff725ed48e3f4ff4e3bd3ba343c40ce5736df
Ambuj Kumar <ambuj67@gmail.com>
2025-10-27 20:49:41 +0530

    Removed deprecated MongoDB options

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 
 // connect to mongoDB
-try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to mongoDB");
-} catch (error) {
-    console.log("Error: ", error);
-}
+// connect to MongoDB
+mongoose.connect(URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB connection error:", error);
+  });
+
 
 // defining routes
 app.use("/book", bookRoute);
